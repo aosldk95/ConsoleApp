1318b9b1625da24496e5f88aa666cab906757dba
aosldk95 <50979465+aosldk95@users.noreply.github.com>
2019-05-31 15:20:58 +0900

    초기 커밋.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal fox = new Animal("fox", "Raaw");
+            fox.MakeSound();
+
+            Animal dog = new Animal("dog", "wal");
+            dog.MakeSound();
+
+            Animal cow = new Animal("cow", "ema");
+            cow.MakeSound();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp/animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Animal
+    {
+        static int legCount;
+
+        public string mName;
+        public string mSound;
+
+        public Animal(string name, string sound)
+        {
+            mName = name;
+            mSound = sound;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", mName, mSound);
+        }
+
+    }
+}
